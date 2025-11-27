391a3c7048b56cfeabd0db75cf59d8e6de9b8934
Kohki SHIKATA <indigoworks@gmail.com>
2025-11-27 11:14:57 +0900

    Add bw command exist or not

--- app/src/main.go
+++ app/src/main.go
@@ -1,8 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/exec"
+)
+
+func checkBwCommand() (bool, string) {
+	path, err := exec.LookPath("bw")
+	if err != nil {
+		return false, ""
+	}
+	return true, path
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	installed, path := checkBwCommand()
+	if !installed {
+		fmt.Fprintf(os.Stderr, "[ERROR] ❌ bw command is not installed...\n")
+		os.Exit(1)
+	}
+	fmt.Printf("[INFO] ✅ bw command is installed! (path: %s)\n", path)
 }
 
